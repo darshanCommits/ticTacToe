937d5fd66650cd05bc95bd708d03fb647cc66501
Darshan Kumawat <kumawatdarshan.1304@gmail.com>
2023-05-08 18:05:35 +0530

    safe save

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
+// A tic tac toe game.
 
-
-function main () {
+function main() {
 	const grid = document.querySelector("#game__grid");
+	let noOfMove = 0;
 
-  (function render () {
-    const fragment = document.createDocumentFragment();
-    for (let i = 0; i < 16; i++) {
-      const gridChildren = document.createElement("div");
-      gridChildren.classList.add("game__child");
-      fragment.appendChild(gridChildren);
-    }
-    grid.appendChild(fragment);
-  })();
+	(function render() {
+		const fragment = document.createDocumentFragment();
+		for (let i = 0; i < 16; i++) {
+			const gridChildren = document.createElement("div");
+			gridChildren.classList.add("game__child");
+			fragment.appendChild(gridChildren);
+		}
+		grid.appendChild(fragment);
+	})();
 
-  
+	function registerMove(e) {
+		const whichMove = noOfMove => (noOfMove % 2 ? "X" : "O");
+		const target = e.target.closest(".game__child");
 
+		if (!target.classList.contains("bruh")) {
+			target.innerText = whichMove(noOfMove++);
+			target.classList.add("bruh");
+		}
+		// target.addEventListener("transitionend", function () {}, { once: true });
+	}
 
+	[...grid.children].forEach(child => {
+		child.addEventListener("click", registerMove);
+	});
 }
+
+main();
