9752ad39eb09ad3989f276fe4b6eef6be0195f04
Darshan Kumawat <kumawatdarshan.1304@gmail.com>
2023-06-03 00:19:25 +0530

    pure function commit 1

--- app.js
+++ app.js
@@ -1,14 +1,117 @@
-// A tic tac toe game.
+const game1 = board();
 const grid = document.querySelector("#game__grid");
 
-function main() {
-	const feature = features();
-	const gameBoard = board();
+main(grid, game1);
 
-	feature.render(grid);
+// Tic Tac Toe game
 
-	let noOfMove = 0;
+function board() {
+	const getGameBoard = () => [[], [], []]; // returns the gameBoard
 
+	const getMove = (board, i, j) => board[i][j]; // gets the value for said index
+	const setMove = (board, i, j, value) => (board[i][j] = value); // sets the value for said index
+
+	return {
+		getMove,
+		setMove,
+		getGameBoard,
+	};
+}
+
+function getWinner(currentBoard) {
+	const isWinner = arr => {
+		if (arr.every(move => move === "X")) return "X";
+		else if (arr.every(move => move === "O")) return "O";
+		else return null;
+	};
+
+	const _checkRow = board => {
+		for (let i = 0; i < 3; i++) {
+			let row = [];
+
+			for (let j = 0; j < 3; j++) row.push(game1.getMove(board, i, j));
+
+			if (isWinner(row) !== null) return isWinner(row);
+		}
+		return null;
+	};
+
+	const _checkColumn = board => {
+		for (let i = 0; i < 3; i++) {
+			let col = [];
+
+			for (let j = 0; j < 3; j++) col.push(game1.getMove(board, j, i));
+
+			if (isWinner(col) !== null) return isWinner(col);
+		}
+		return null;
+	};
+
+	const _checkDiagonal = board => {
+		let diagonal1 = [];
+		let diagonal2 = [];
+
+		for (let i = 0; i < 3; i++) {
+			diagonal1.push(game1.getMove(board, i, i));
+			diagonal2.push(game1.getMove(board, i, 2 - i));
+		}
+
+		return isWinner(diagonal1) || isWinner(diagonal2);
+	};
+
+	const _checkTie = board => {
+		if (
+			![...grid.children].every(child => child.classList.contains("changed")) ||
+			ans !== null
+		)
+			return null;
+
+		for (let i = 0; i < 3; i++) {
+			for (let j = 0; j < 3; j++)
+				if (game1.getMove(board, i, j) === "") return null;
+		}
+
+		return "Tied";
+	};
+
+	const ans =
+		_checkColumn(currentBoard) ||
+		_checkRow(currentBoard) ||
+		_checkDiagonal(currentBoard);
+
+	return ans || _checkTie(currentBoard);
+}
+
+function features() {
+	const render = $grid => {
+		const _createGameChild = (i, j) => {
+			const gridChildren = document.createElement("div");
+			gridChildren.classList.add("game__child");
+			gridChildren.dataset.row = i;
+			gridChildren.dataset.column = j;
+
+			return gridChildren;
+		};
+
+		const fragment = document.createDocumentFragment();
+		for (let i = 0; i < 3; i++) {
+			for (let j = 0; j < 3; j++) {
+				const child = _createGameChild(i, j);
+				fragment.appendChild(child);
+			}
+		}
+		$grid.appendChild(fragment);
+	};
+
+	return {
+		render,
+	};
+}
+
+function main(grid, currentBoard) {
+
+
+	
 	const registerMove = e => {
 		const register = e => {
 			const target = e.target.closest(".game__child");
@@ -19,7 +122,7 @@ function main() {
 			if (!target.classList.contains("changed")) {
 				target.innerText = whichMove(noOfMove);
 
-				gameBoard.setMove(row, col, whichMove(noOfMove));
+				currentBoard.setMove(gameBoard, row, col, whichMove(noOfMove));
 
 				target.classList.add("changed");
 			}
@@ -42,137 +145,4 @@ function main() {
 		register(e);
 		noOfMove++;
 	};
-
-	[...grid.children].forEach(child =>
-		child.addEventListener("click", registerMove, { once: true })
-	);
 }
-function board() {
-	let gameBoard = [[], [], []];
-
-	const getGameBoard = () => gameBoard; // returns the gameBoard
-	const getMove = (i, j) => gameBoard[i][j]; // gets the value for said index
-	const setMove = (i, j, value) => (gameBoard[i][j] = value); // sets the value for said index
-
-	const resetBoard = () => {
-		for (let i = 0; i < 3; i++) {
-			for (let j = 0; j < 3; j++) {
-				gameBoard[i][j] = "";
-			}
-		}
-	};
-
-	return {
-		getMove,
-		setMove,
-		resetBoard,
-		getGameBoard,
-	};
-}
-
-function getWinner(gameBoard) {
-	const isWinner = arr => {
-		if (arr.every(move => move === "X")) return "X";
-		else if (arr.every(move => move === "O")) return "O";
-		else return null;
-	};
-
-	const _checkRow = () => {
-		for (let i = 0; i < 3; i++) {
-			let row = [];
-
-			for (let j = 0; j < 3; j++) row.push(gameBoard.getMove(i, j));
-
-			if (isWinner(row) !== null) return isWinner(row);
-		}
-		return null;
-	};
-
-	const _checkColumn = () => {
-		for (let i = 0; i < 3; i++) {
-			let col = [];
-
-			for (let j = 0; j < 3; j++) col.push(gameBoard.getMove(j, i));
-
-			if (isWinner(col) !== null) return isWinner(col);
-		}
-		return null;
-	};
-
-	const _checkDiagonal = () => {
-		let diagonal1 = [];
-		let diagonal2 = [];
-
-		for (let i = 0; i < 3; i++) {
-			diagonal1.push(gameBoard.getMove(i, i));
-			diagonal2.push(gameBoard.getMove(i, 2 - i));
-		}
-
-		if (isWinner(diagonal1) !== null) return isWinner(diagonal1);
-		else if (isWinner(diagonal2) !== null) return isWinner(diagonal2);
-		else return null;
-	};
-
-	const _checkTie = () => {
-		if (
-			![...grid.children].every(child => child.classList.contains("changed")) ||
-			ans !== null
-		)
-			return null;
-
-		for (let i = 0; i < 3; i++) {
-			for (let j = 0; j < 3; j++)
-				if (gameBoard.getMove(i, j) === "") return null;
-		}
-
-		return "Tied";
-	};
-
-	let ans = _checkColumn() || _checkRow() || _checkDiagonal();
-
-	return ans || _checkTie();
-}
-
-// WIP
-
-function features() {
-	const render = grid => {
-		const fragment = document.createDocumentFragment();
-		for (let i = 0; i < 3; i++) {
-			for (let j = 0; j < 3; j++) {
-				const gridChildren = document.createElement("div");
-				gridChildren.classList.add("game__child");
-				gridChildren.dataset.row = i;
-				gridChildren.dataset.column = j;
-
-				fragment.appendChild(gridChildren);
-			}
-		}
-		grid.appendChild(fragment);
-	};
-
-	const newGame = () => {
-		gameBoard.resetBoard();
-		const grid = document.querySelector("#game__grid");
-
-		winnerTab.classList.remove("visible");
-		winnerName.innerText = "";
-
-		[...grid.children].forEach(x => {
-			x.innerText = "";
-			x.classList.remove("changed");
-		});
-	};
-
-	const renderWinner = winner => {
-		winnerTab.classList.add("visible");
-		winnerName.innerText = winner;
-	};
-
-	return {
-		render,
-		newGame,
-		renderWinner,
-	};
-}
-main();
